fcde137ed21bce9307df7af43a85e8f1a3601040
khader12345 <147363653+khader12345@users.noreply.github.com>
2025-11-14 11:42:01 -0500

    Demo

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, Dockerized Node.js App!');
+  res.send('Hello, Dockerized Node.js App Test for demo!');
 });
 
 app.listen(PORT, () => {
